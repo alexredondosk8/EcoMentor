5dadf5a40801e82d1489fecdb027306445a794d2
Alexander Redondo <alexredondosk8@gmail.com>
2025-04-21 14:32:39 -0500

    Update a_page.py

--- pages/a_page.py
+++ pages/a_page.py
@@ -1,1 +1,9 @@
-
+st.markdown(
+    """
+    <div style='text-align: center;'>
+        <h1>🌿 Bienvenido/a a EcoMentor 🌿</h1>
+        <p>Tu asistente personal para vivir de forma más sostenible a_page.</p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
